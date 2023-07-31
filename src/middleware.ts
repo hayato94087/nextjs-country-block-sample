ffbc777b8ddcb76670a13beb58dc1880cea61e3a
hayato94087 <hayato94087@gmail.com>
2023-08-01 05:28:29 +0900

    日本以外のアクセスを拒否する

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+// middleware.ts
+import { NextResponse, type NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const country = request.geo?.country;
+
+  // 接続元の国を表示
+  console.info(country);
+
+  if (process.env.NODE_ENV === "production") {
+    if (country && country !== "JP") {
+      console.info(
+        `IPアドレスが日本以外のため、アクセスを拒否しました。[request.ip = ${request.ip}]`
+      );
+      return new NextResponse(null, { status: 401 });
+    }
+  }
+}
